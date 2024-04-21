cc8409c544e6a0fe7392f2e95eccc308b3774416
zaher1307 <ahmedzaher1307@gmail.com>
2024-04-21 11:36:26 +0200

    push updates

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local previewers = require("telescope.previewers")
 local Job = require("plenary.job")
+local lga_actions = require("telescope-live-grep-args.actions")
 
 local new_maker = function(filepath, bufnr, opts)
     filepath = vim.fn.expand(filepath)
@@ -23,7 +24,23 @@ end
 require("telescope").setup {
     defaults = {
         buffer_previewer_maker = new_maker,
+    },
+    extensions = {
+        live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            -- define mappings, e.g.
+            mappings = { -- extend mappings
+            i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+        },
+        -- ... also accepts theme settings, for example:
+        -- theme = "dropdown", -- use dropdown theme
+        -- theme = { }, -- use own theme spec
+        -- layout_config = { mirror=true }, -- mirror preview pane
     }
+}
 }
 
 
@@ -62,5 +79,7 @@ end)
 vim.keymap.set("n", "<leader>lg", function()
     require('telescope.builtin').live_grep()
 end, { desc = "Live Grep" })
+
+vim.keymap.set("n", "<leader>la", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
 vim.api.nvim_set_keymap('n', '<leader>se', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
